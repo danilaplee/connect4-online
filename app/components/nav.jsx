b96562ce6fcccd5e2f1579de17af30fc9a86f9df
danilaplee <fiend.of.sound@gmail.com>
2016-07-03 21:46:50 +0300

    added restart title

--- app/components/nav.jsx
+++ app/components/nav.jsx
@@ -9,8 +9,8 @@ export default React.createClass({
 		else this.props.game.startGame();
 		setTimeout(function()
 		{
-			self.props.restart = {}
-			self.props.restart[mode] = true;
+			self.state.restart = {}
+			self.state.restart[mode] = "Restart";
 
 		}, 1000)
 	},
@@ -22,13 +22,13 @@ export default React.createClass({
 		}
 	    return <ul className="list-group">
 			<li className="list-group-item">
-				<a href="#" onClick={handleClick.bind(this,'single')}>{this.props.restart.single || 'Play'} Singleplayer Game</a>
+				<a href="#" onClick={handleClick.bind(this,'single')}>{this.state.restart.single || 'Play'} Singleplayer Game</a>
 			</li>
 			<li className="list-group-item">
-			    <a href="#" onClick={handleClick.bind(this,'multi')}>{this.props.restart.multi || 'Play'} Multiplayer Game</a>
+			    <a href="#" onClick={handleClick.bind(this,'multi')}>{this.state.restart.multi || 'Play'} Multiplayer Game</a>
 			</li>
 			<li className="list-group-item">
-			   <a href="#" onClick={handleClick.bind(this,'hot')}>{this.props.restart.hot || 'Play'} Hotseat Game</a>
+			   <a href="#" onClick={handleClick.bind(this,'hot')}>{this.state.restart.hot || 'Play'} Hotseat Game</a>
 			</li>
 			<li className="list-group-item">
 			   <a href="#" onClick={this.props.game.openColorDialog}>Settings</a>
